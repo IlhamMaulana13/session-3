ba68de615ed1d9c13ed73b95b121791e5b44731d
Ilhmmlna <mhmmad13ilhammlna@gmail.com>
2025-10-03 21:10:23 +0700

    initial new project dart

---
+++ bin/map.dart
@@ -0,0 +1,10 @@
+void main() { 
+ Map<String, String> user = { 
+ 'id': '001', 
+ 'nama': 'Budi', 
+ 'email': 'budi@mail.com' 
+ }; 
+ print(user['nama']); // Budi 
+ user['email'] = 'budi@example.com'; // update value 
+ print(user); 
+} 
---
+++ test/session_3_practice_test.dart
@@ -0,0 +1,8 @@
+import 'package:session_3_practice/session_3_practice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
