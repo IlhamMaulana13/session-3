8422833429282c42132ce9eedbe6386ee2039f28
Ilhmmlna <mhmmad13ilhammlna@gmail.com>
2025-10-03 21:23:35 +0700

    menghapus file tidak perlu

--- bin/session_3_practice.dart
+++ bin/session_3_practice.dart
@@ -1,7 +1,22 @@
+class Person { 
+ final String _firstName; 
+ final String _lastName; 
+ final String _phone; 
+ Person(this._firstName, this._lastName, this._phone); 
+ @override 
+ toString() { 
+ return "$_firstName $_lastName $_phone"; 
+ } 
+} 
 void main() { 
- var angka = {1, 2, 3, 3}; // otomatis {1, 2, 3} 
- print(angka); 
- angka.add(4); 
- angka.add(2); // tidak ditambahkan karena sudah ada 
- print(angka); // {1, 2, 3, 4} 
+ List<Person> list = [ 
+ Person("Mark", "Clow", "4043124462"), 
+ Person("Brant", "Sandermine", "4243124462"), 
+ Person("Phillip", "Perry", "4243124444") 
+ ]; 
+ print("Not sorted: $list"); 
+ list.sort((a, b) => a._firstName.compareTo(b._firstName)); 
+ print("Sorted by first name: $list"); 
+ list.sort((a, b) => a._firstName.compareTo(b._lastName)); 
+ print("Sorted by last name: $list"); 
 } 
--- test/session_3_practice_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:session_3_practice/session_3_practice.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
